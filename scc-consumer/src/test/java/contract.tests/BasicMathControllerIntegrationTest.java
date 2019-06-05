2ebb9b712719f58c932be4fe0f0e5961d35f0943
kateryna <kateryna.podobraya@gmail.com>
2019-06-05 11:31:49 +0300

    Fixed pom.xml.

--- scc-consumer/src/test/java/contract.tests/BasicMathControllerIntegrationTest.java
+++ scc-consumer/src/test/java/contract.tests/BasicMathControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package contract.tests;
 
+import controller.SpringCloudContractConsumerApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(classes = {SpringCloudContractConsumerApplication.class},webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureStubRunner(stubsMode = StubRunnerProperties.StubsMode.LOCAL,
--- scc-consumer/src/test/java/integration/SpringContextIntegrationTest.java
+++ scc-consumer/src/test/java/integration/SpringContextIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.baeldung.spring.cloud.springcloudcontractconsumer.SpringCloudContractConsumerApplication;
+import controller.SpringCloudContractConsumerApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringCloudContractConsumerApplication.class)
