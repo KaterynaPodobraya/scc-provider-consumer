a1ba0ea10f08f4ea0882d98c849d53abe831359c
kateryna <kateryna.podobraya@gmail.com>
2019-06-07 21:39:14 +0300

    Adds Draft documents generation.

--- scc-producer/src/test/java/test/java/producer/BaseTestClass.java
+++ scc-producer/src/test/java/test/java/producer/BaseTestClass.java
@@ -1,27 +1,36 @@
 package test.java.producer;
 
 import controller.EvenOddController;
+import controller.Greetingcontroller;
 import controller.SCCProducerApplication;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={SCCProducerApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-//@DirtiesContext
+@DirtiesContext
+@AutoConfigureMessageVerifier
+//@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class BaseTestClass {
 
     @Autowired
     private EvenOddController evenOddController;
 
+    @Autowired
+    private Greetingcontroller greetingController;
+
     @Before
     public void setup() {
-        StandaloneMockMvcBuilder standaloneMockMvcBuilder = MockMvcBuilders.standaloneSetup(evenOddController);
-        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+        StandaloneMockMvcBuilder standaloneMockMvcBuilderForEvenOdd = MockMvcBuilders.standaloneSetup(evenOddController, greetingController);
+        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilderForEvenOdd);
     }
 }
